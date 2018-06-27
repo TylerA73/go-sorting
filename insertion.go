fad1d81183ca23e72806ab8782e7eb939ba1e121
arseneaultt2 <arseneaultt2@mymacewan.ca>
2018-06-27 10:49:32 -0600

    Insertion sort completed

--- insertion.go
+++ insertion.go
@@ -4,6 +4,23 @@ import "fmt"
 
 func insertionSort(a []int) {
 
-	var tmp int
+	// Print out the initial array
+	fmt.Println(a)
+
+	// We start at one position ahead of the front of the array
+	// This is because insertion sort works backwards, and we have nothing before the front to compare the front to
+	for i := 1; i < len(a); i++ {
+		tmp := a[i] // tmp becomes the next value at position i for comparison
+
+		// While j >= 0 and the value at index j is greater than tmp, continue moving the values forward one spot
+		for j = i - 1; j >= 0 && a[j] > tmp; j-- {
+			a[j+1] = a[j]
+		}
+
+		a[j+1] = tmp
+
+		// Print out the next step to show that insertion is working as it should
+		fmt.Println(a)
+	}
 
 }
--- selection.go
+++ selection.go
@@ -3,25 +3,35 @@ package main
 import "fmt"
 
 func selectionSort(a []int) {
-	var index int
-	var tmp int
 
+	var index int // Index of the smallest value
+	var tmp int   // Temporary variable used for swapping
+
+	// Print the initial array
 	fmt.Println(a)
 
+	// Loop through the entire array once
 	for i := 0; i < len(a)-1; i++ {
-		tmp = a[i]
-		index = i
+		tmp = a[i] // The temporary variable becomes the next value we are comparing initially
+		index = i  // index becomes the index of that temporary value
 
+		// Loop through the array starting from 1 position in front of our i index to compare all values to it
+		// Everything behind the i index is sorted
 		for j := i + 1; j < len(a); j++ {
+
+			// if tmp is larger than the current compared value in the array, then we have a new smallest value
 			if tmp > a[j] {
 				tmp = a[j]
 				index = j
 			}
 		}
 
+		// Swap the current value at i with the next smallest value
+		// If the smallest value is already in this location, the swap will just put it back where it was
 		a[index] = a[i]
 		a[i] = tmp
 
+		// Print the array at each step to show that it works like selection sort should
 		fmt.Println(a)
 	}
 }
